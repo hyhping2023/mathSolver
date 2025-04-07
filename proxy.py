8bcdc58524ad61357412ad84edd177d730dfc9a4
hyhping2023 <yhuang704@connect.hkust-gz.edu.cn>
2025-04-07 22:11:36 +0800

    finsh proxy

--- proxy.py
+++ proxy.py
@@ -3,17 +3,26 @@ import argparse
 import uvicorn
 import requests
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Dict, Any
 
 app = FastAPI()
 host = "10.120.20.225"
 port = 9000
 
-@app.get("/query")
-def proxy(message):
+class Message(BaseModel):
+    query: str
+    query_type: str
+    query_args: dict
+
+@app.post("/query")
+def proxy(message: Message):
     """
     A decorator to proxy a function call.
     """
-    return requests.post(f"http://{host}:{port}/query", json=message)
+    # message = message.model_dump("json")
+    message = {"query": message.query, "query_type": message.query_type, "query_args": message.query_args}
+    return requests.post(f"http://{host}:{port}/query", json=message).json()
 
 if __name__ == "__main__":
     uvicorn.run(app, port=30027, host="0.0.0.0")    
